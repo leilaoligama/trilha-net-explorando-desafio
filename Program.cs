6fdd0bb25344de5ae84e84657a457a85536b9faa
Leila Gama <leilagama@gmail.com>
2024-05-20 21:54:38 -0300

    Finalização do desafio

--- Program.cs
+++ Program.cs
@@ -12,12 +12,13 @@ Pessoa p3 = new Pessoa(nome: "Dermival Gama");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+//hospedes.Add(p3);
 
 // Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 12);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
